9d034425d943590add3671619e87931da5fd44e9
Mykhailo Petrenko <mihalytch@gmail.com>
2020-03-23 01:04:54 +0200

    Install Angular Material as a dependency

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OperationHandlerService } from './api/services/operation-handler.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cards';
+
+  constructor(
+    private operationHandlerService: OperationHandlerService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.operationHandlerService.handleUsingGET1Response().toPromise()
+      .then((a) => {
+        console.log(a);
+      });
+  }
 }
---
+++ web-ui/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+  ]
+})
+export class SharedModule { }
